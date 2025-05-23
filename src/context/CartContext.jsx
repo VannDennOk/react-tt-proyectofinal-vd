4556cc0c3b2e837c3cf2f0544392c85453e781b5
Vanina Denegri <denegri.vanina@gmail.com>
2025-05-23 02:00:07 -0300

    Lógica del Login

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,13 +2,14 @@ import { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext()
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([])
   const [productos, setProductos] = useState([])
   const [error, setError] = useState(false)
   const [carga, setCarga] = useState(true)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+  //conecta MockAPI
   useEffect(() => {
     fetch('https://681455e4225ff1af162889a4.mockapi.io/productos-ecommerce/product')
       .then(respueta => respueta.json())
@@ -25,11 +26,13 @@ export const CartProvider = ({children}) => {
       });
   }, [])
 
+  //Maneja la apertura del acrrito
   const [isCartOpen, setCartOpen] = useState(false)
   useEffect(() => {
     document.body.style.overflow = isCartOpen ? 'hidden' : 'auto';
   }, [isCartOpen]);
 
+  //Maneja el botón agregar al carrito (con mensaje modal si ya está agregado)
   const [modalAbierto, setModalAbierto] = useState(false);
   const [mensajeModal1, setMensajeModal1] = useState('');
   const [mensajeModal2, setMensajeModal2] = useState('');
@@ -45,6 +48,7 @@ export const CartProvider = ({children}) => {
     }
   }
 
+  //actualiza la cantidad en el carrito
   const actualizarCantidad = (id, nuevaCantidad) => {
     setCart(prevCart =>
       prevCart.map(item =>
@@ -55,42 +59,46 @@ export const CartProvider = ({children}) => {
     );
   };
 
+  //maneja el botón borrar producto (icono de tacho de basura)
   const borrarProducto = (product) => {
     setCart(cart.filter(item => item.id != product.id))
   }
 
+  //maneja el botón vaciar el carrito (elimina a todo lo que haya en el carrito)
   const vaciarCarrito = () => {
     setCart([])
   }
 
+  //maneja e contador
   const cartCount = () => {
     return cart.reduce((total, item) => total + item.cantidad, 0)
   }
 
 
-    return (
-        <CartContext.Provider 
-        value={
-            { 
-            cart, 
-            productos, 
-            error, 
-            carga,
-            isAuthenticated,
-            vaciarCarrito,
-            handleAddToCart,
-            borrarProducto,
-            isCartOpen,
-            setCartOpen,
-            actualizarCantidad,
-            cartCount,
-            modalAbierto,
-            mensajeModal1,
-            mensajeModal2,
-            setModalAbierto
-            }
-        }> 
-        {children}
-        </CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider
+      value={
+        {
+          cart,
+          productos,
+          error,
+          carga,
+          isAuthenticated,
+          setIsAuthenticated,
+          vaciarCarrito,
+          handleAddToCart,
+          borrarProducto,
+          isCartOpen,
+          setCartOpen,
+          actualizarCantidad,
+          cartCount,
+          modalAbierto,
+          mensajeModal1,
+          mensajeModal2,
+          setModalAbierto
+        }
+      }>
+      {children}
+    </CartContext.Provider>
+  )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,98 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { CartContext } from "../context/CartContext"
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+  const { setIsAuthenticated } = useContext(CartContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState({})
+  const navigate = useNavigate()
+
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+     //Analisis de que no esté vacío
+    let validationError = {}
+    if (!email) validationError.email = "Es necesario ingresar un e-mail válido"
+    if (!password) validationError.password = "Es necesario ingresar una contraseña"
+
+    if (Object.keys(validationError).length > 0) {
+      setError(validationError)
+      return
+    }
+
+    //llama al JSON
+    try {
+      const res = await fetch('data/users.json');
+      const users = await res.json();
+
+      const foundUser = users.find((user) => user.email === email && user.password === password)
+
+      if (!foundUser) {
+        setError({ email: 'credenciales inválidas' })
+      } else {
+        console.log('User role:', foundUser.role);
+        if (foundUser.role === 'admin') {
+          setIsAuthenticated(true);
+          navigate('/admin')
+        } else {
+          navigate('/')
+        }
+      }
+
+    } catch (err) {
+      console.error('Error fetching users:', err);
+      setError({ email: 'Algo salió mal, por favor intentalo más tarde' })
+    }
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form className='form_container' onSubmit={handleSubmit} >
+      <div className='form_container-box'>
+        <label
+          className='form_container-label'
+          htmlFor="formBasicEmail">
+          E-mail
+        </label>
+        <input
+          className='form_container-input'
+          id='formBasicEmail'
+          type="email"
+          placeholder='Ingresá tu e-mail'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {error.email && (
+          <div className='form_error'>{error.email}</div>
+        )}
+      </div>
+
+      <div className='form_container-box'>
+        <label
+          className='form_container-label'
+          htmlFor="formBasicPassword">
+          Contraseña
+        </label>
+        <input
+          className='form_container-input'
+          id='formBasicPassword'
+          type="password"
+          placeholder='Ingresá tu contraseña'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error.password && (
+          <div className='form_error'>{error.password}</div>
+        )}
+      </div>
+
+      <button
+        type='submit'
+      >Ingresar</button>
+    </form>
   )
 }
 
