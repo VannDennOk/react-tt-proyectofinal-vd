548349b033197940da51138be8972f6242e05ca2
Vanina Denegri <denegri.vanina@gmail.com>
2025-05-24 18:46:09 -0300

    Actualiza diseño de galería de imágenes

---
+++ src/assets/datacarrusel.js
@@ -0,0 +1,20 @@
+export const datacarrusel = [
+    {
+        original: "src/assets/Carrusel/2x700.png",
+        alt: "imagen de producto 2"
+    
+    },
+    {
+        original: "src/assets/Carrusel/3x700.png",
+        originalAlt: "imagen de producto 3"
+    
+    },
+    {
+        original: "src/assets/Carrusel/4x700.png",
+        originalAlt: "imagen de producto 4"
+    },
+    {
+        original: "src/assets/Carrusel/5x700.png",
+        originalAlt: "imagen de producto 5"
+    }
+]
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,25 +1,44 @@
 import React from 'react'
+import ImageGallery from 'react-image-gallery'
+import "react-image-gallery/styles/css/image-gallery.css";
+import { datacarrusel } from '../assets/datacarrusel';
 import './styles/Gallery.css'
 
 const Gallery = () => {
 
-  const images = [
-    "https://i.postimg.cc/c4Q89ZZs/2x700.png",
-    "https://i.postimg.cc/VvtbcPCW/3x700.png",
-    "https://i.postimg.cc/pV5jjz65/4x700.png",
-    "https://i.postimg.cc/yYqZyxMQ/5x700.png"
-  ];
+  /*   const images = [
+      "https://i.postimg.cc/c4Q89ZZs/2x700.png",
+      "https://i.postimg.cc/VvtbcPCW/3x700.png",
+      "https://i.postimg.cc/pV5jjz65/4x700.png",
+      "https://i.postimg.cc/yYqZyxMQ/5x700.png"
+    ]; */
 
   return (
-    <section className='gallery_container'>
-      <div className='gallery_slider'> 
+    <>
+      {/*       <div> 
           {
             images.map((src, index) => (
-              <img className='gallery_img' key={index} src={src} alt={`imagen${index + 1}`} />
+              <img key={index} src={src} alt={`imagen${index + 1}`} />
             ))
           }
+      </div> */}
+      
+      <div className='gallery_container'>
+        <ImageGallery
+          items={datacarrusel}
+          infinite={true}
+          autoPlay={true}
+          showPlayButton={false}
+          showFullscreenButton={false}
+          showThumbnails={false}
+          showNav={false}
+          showBullets={true}
+          slideInterval={3000}
+          slideDuration={1000}
+        />
       </div>
-    </section>
+
+    </>
   )
 }
 
