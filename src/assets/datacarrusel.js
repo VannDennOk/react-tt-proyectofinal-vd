b5e3523b37b6300171622a727205b150b8b5778c
Vanina Denegri <denegri.vanina@gmail.com>
2025-05-24 20:33:52 -0300

    Actualiza links de imágenes y elimina las en desuso

--- src/assets/datacarrusel.js
+++ src/assets/datacarrusel.js
@@ -1,20 +1,20 @@
 export const datacarrusel = [
     {
-        original: "src/assets/Carrusel/2x700.png",
-        alt: "imagen de producto 2"
+        original: "https://i.postimg.cc/c4Q89ZZs/2x700.png",
+        originalAlt: "imagen de producto 2"
     
     },
     {
-        original: "src/assets/Carrusel/3x700.png",
+        original: "https://i.postimg.cc/VvtbcPCW/3x700.png",
         originalAlt: "imagen de producto 3"
     
     },
     {
-        original: "src/assets/Carrusel/4x700.png",
+        original: "https://i.postimg.cc/pV5jjz65/4x700.png",
         originalAlt: "imagen de producto 4"
     },
     {
-        original: "src/assets/Carrusel/5x700.png",
+        original: "https://i.postimg.cc/yYqZyxMQ/5x700.png",
         originalAlt: "imagen de producto 5"
     }
 ]
