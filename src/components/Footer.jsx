4dea51988f313defa3cb80bb141a573a4cf2230c
Vanina Denegri <denegri.vanina@gmail.com>
2025-05-25 01:51:59 -0300

    Actualiza diseño Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,52 +6,52 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTiktok, faInstagram, faYoutube, faCcVisa, faCcMastercard, faCcPaypal, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
-
 const Footer = () => {
   return (
     <footer>
       <section className='footer_container-top'>
         <div className='footer_data'>
-          <h2><img className='footer_logo' src={logo} alt='logo liv' /></h2>
+          <Link to="/"><img className='logo' src={logo} alt='logo liv' /></Link>
           <p>Gomitas divertidas que combinan ciencia y sabor para cuidarte todo el día. Sin azúcar añadida. 100% veganas.</p>
         </div>
         <div className='footer_data'>
           <h3>Envíos</h3>
-          <p>Envíos expres en CABA y GBA</p>
-          <p>Envíos a toda la Argentina en 72hs</p>
+          <p>Envíos expres en CABA y GBA. Envíos a toda la Argentina en 72hs.</p>
         </div>
         <div className='footer_data'>
           <h3>Contacto</h3>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faLocationDot} /><p>CABA, Argentina</p></span>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faEnvelope} /><p>mail@mail.com</p></span>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faWhatsapp} /><p>+54 9 11 68611033</p></span>
+          <a href="https://maps.app.goo.gl/819yPqRG959KTDhXA" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='icon-big' icon={faLocationDot} />CABA, Argentina</a>
+          <a href="mailto:vann.denn@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='icon-big' icon={faEnvelope} />vann.denn@gmail.com</a>
+          <a href="https://wa.me/5491168611033" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className='icon-big' icon={faWhatsapp} />+54 9 11 68611033</a>
+
         </div>
         <div className='footer_data'>
           <h3>Navegación</h3>
-              <Link className='footernav_link' to='/'>Inicio</Link>
-              <Link className='footernav_link' to='/productos'>Productos</Link>
-              <Link className='footernav_link' to='/nosotros'>Nosotros</Link>
-              <Link className='footernav_link' to='/contacto'>Contacto</Link>
+          <ul>
+            <li><Link to='/'>Inicio</Link></li>
+            <li><Link to='/productos'>Productos</Link></li>
+            <li><Link to='/nosotros'>Nosotros</Link></li>
+            <li><Link to='/contacto'>Contacto</Link></li>
+          </ul>
         </div>
         <div className='footer_data'>
           <h3>Medios de pago</h3>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faCcVisa} /><p>Visa</p></span>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faCcMastercard} /><p>Mastercard</p></span>
-          <span className='footer_line'><FontAwesomeIcon className='footer_icon' icon={faCcPaypal} /> <p>PayPal</p></span>
+          <span><FontAwesomeIcon className='icon-big' icon={faCcVisa} /><p>Visa</p></span>
+          <span><FontAwesomeIcon className='icon-big' icon={faCcMastercard} /><p>Mastercard</p></span>
+          <span><FontAwesomeIcon className='icon-big' icon={faCcPaypal} /><p>PayPal</p></span>
         </div>
         <div className='footer_social'>
-          <button className='footer_social-icon'><FontAwesomeIcon icon={faTiktok} /></button>
-          <button className='footer_social-icon'><FontAwesomeIcon icon={faInstagram} /></button>
-          <button className='footer_social-icon'><FontAwesomeIcon icon={faYoutube} /></button>
+          <button><FontAwesomeIcon icon={faTiktok} /></button>
+          <button><FontAwesomeIcon icon={faInstagram} /></button>
+          <button><FontAwesomeIcon icon={faYoutube} /></button>
         </div>
       </section>
 
       <section className='footer_container-bottom'>
-        <p className='txt_blanco'>© Copyright 2025 - Vanina Denegri</p>
+        <p className='txt_blanco'>© Copyright Vanina Denegri 2025</p>
         <p className='txt_blanco'>Proyecto Final Integrador del curso de React de Talento Tech.</p>
         <p className='txt_blanco'>Página creada con propósitos educativos y sin fines de lucro.</p>
       </section>
-
     </footer>
   )
 }
