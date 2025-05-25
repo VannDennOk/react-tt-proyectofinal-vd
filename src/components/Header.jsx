7bb9f18811eb57b93c1ca2fadb17a037565e35d7
Vanina Denegri <denegri.vanina@gmail.com>
2025-05-25 02:24:07 -0300

    Actualiza Estilos Header. Actualiza estilos Banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,20 @@ import { Link } from 'react-router-dom';
 const Header = ({ vaciarCarrito, cartItems, cartCount, setCartOpen, isCartOpen, borrarProducto, actualizarCantidad }) => {
 
   return (
-    <header className='header_container'>
-      <div className='header_container_top'>
-        <h1><img className='header_logo' src={logo} alt='logo liv' /></h1>
-        <span className='link_icon_container'>
-          <a className='link_icon' href="#"><FontAwesomeIcon icon={faMagnifyingGlass} />Buscar</a>
-          <Link className='link_icon'to='/login'><FontAwesomeIcon icon={faUser} />Login</Link>
-          <a className='link_icon' href="#"><FontAwesomeIcon icon={faHeart} />Favs</a>
-          <a className='link_icon' href="#" onClick={() => setCartOpen(true)}>
+    <header>
+      <div className='header_container'>
+        <h1><img className='logo' src={logo} alt='logo liv' /></h1>
+        <div className='header_links'>
+          <button><FontAwesomeIcon icon={faMagnifyingGlass}/>Buscar</button>
+          <Link to='/login'><FontAwesomeIcon icon={faUser}/>Login</Link>
+          <button><FontAwesomeIcon icon={faHeart} />Favs</button>
+          <button onClick={() => setCartOpen(true)}>
             <FontAwesomeIcon icon={faCartShopping} />
             Carrito{cartCount > 0 && <span className='contador_carrito'>{cartCount}</span>}
-          </a>
-        </span>
+          </button>
+        </div>
       </div>
+    
       <Cart
         vaciarCarrito={vaciarCarrito}
         cartItems={cartItems}
@@ -31,6 +32,7 @@ const Header = ({ vaciarCarrito, cartItems, cartCount, setCartOpen, isCartOpen, 
         borrarProducto={borrarProducto}
         actualizarCantidad={actualizarCantidad} />
       <Nav />
+    
     </header>
   );
 };
