4b4e6dcfa844461ce72a64b48e82f6382783d0e9
Vanina Denegri <denegri.vanina@gmail.com>
2025-06-10 00:13:41 -0300

    Inicio Formulario de Productos (clase 9-10)

---
+++ src/components/FormularioProducto.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react'
+
+const FormularioProducto = ({ onAgregar }) => {
+    const [producto, setProducto] = useState({
+        name: '',
+        description: '',
+        promo: '',
+        price: '',
+        stock: '',
+        ingredients: '',
+        use: '',
+        category: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setProducto({ ...producto, [name]: value })
+    }
+
+    const validarFormulario = () => {
+        const newErrors = {};
+        if (!producto.name.trim()) {
+            newErrors.name = 'Nombre es obligatorio.';
+        }
+        if (!producto.description.trim() || producto.description.length < 10) {
+            newErrors.description = 'Descripción debe tener al menos 10 caracteres.';
+        }
+        if (!producto.price || producto.precio <= 0) {
+            newErrors.price = 'Precio debe ser mayor a 0.';
+        }
+        if (!producto.stock || producto.stock <= 0) {
+            newErrors.stock = 'Stock debe ser mayor a 0.';
+        }
+        if (!producto.ingredients.trim() || producto.ingredients.length < 10) {
+            newErrors.ingredientes = 'Ingredientes debe tener al menos 10 caracteres.';
+        }
+        if (!producto.use.trim() || producto.use.length < 10) {
+            newErrors.use = 'Uso debe tener al menos 10 caracteres.';
+        }
+        if (!producto.category.trim()) {
+            newErrors.category = 'Categoría es obligatorio.';
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!validarFormulario()) {
+            return;
+        }
+        onAgregar(producto)
+        setProducto({
+            name: '',
+            description: '',
+            promo: '',
+            price: '',
+            stock: '',
+            ingredients: '',
+            use: '',
+            category: ''
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Agregar Producto</h2>
+            <div>
+                <label>Nombre</label>
+                <input
+                    type="text"
+                    name='name'
+                    value={producto.name}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+            </div>
+            <div>
+                <label>Descripción</label>
+                <input
+                    type="text"
+                    name='description'
+                    value={producto.description}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.description && <p style={{ color: 'red' }}>{errors.description}</p>}
+            </div>
+            <div>
+                <label>Promo</label>
+                <input
+                    type="text"
+                    name='promo'
+                    value={producto.promo}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.promo && <p style={{ color: 'red' }}>{errors.promo}</p>}
+            </div>
+            <div>
+                <label>Precio</label>
+                <input
+                    type="number"
+                    name='price'
+                    value={producto.price}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.price && <p style={{ color: 'red' }}>{errors.price}</p>}
+            </div>
+            <div>
+                <label>Stock</label>
+                <input
+                    type="number"
+                    name='stock'
+                    value={producto.stock}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.stock && <p style={{ color: 'red' }}>{errors.stock}</p>}
+            </div>
+
+            <div>
+                <label>Ingredientes</label>
+                <input
+                    type="text"
+                    name='ingredients'
+                    value={producto.ingredients}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.ingredients && <p style={{ color: 'red' }}>{errors.ingredients}</p>}
+            </div>
+
+            <div>
+                <label>Uso</label>
+                <input
+                    type="text"
+                    name='use'
+                    value={producto.use}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.use && <p style={{ color: 'red' }}>{errors.use}</p>}
+            </div>
+
+                        <div>
+                <label>Categoria</label>
+                <input
+                    type="text"
+                    name='use'
+                    value={producto.category}
+                    onChange={handleChange}
+                    required
+                />
+                {errors.category && <p style={{ color: 'red' }}>{errors.category}</p>}
+            </div>
+
+            <button className='btn-negro' type="submit">Agregar nuevo producto</button>
+        </form>
+    )
+}
+
+export default FormularioProducto
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './styles/pages.css'
+import logo from '../assets/Img/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket, faPen, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
+import FormularioProducto from '../components/FormularioProducto';
+import { Link } from 'react-router-dom';
 
 const Admin = () => {
+  const [products, setProducts] = useState([]);
+  const [form, setForm] = useState({ id: null, name: "", price: "" });
+  const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(() => {
+          setProducts(data);
+          setLoading(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+        setError(true);
+        setLoading(false);
+      })
+  }, []);
+
+  const agregarProducto = async (producto) => {
+    try {
+      const respuesta = await fetch('https://68476daeec44b9f3493d0ddc.mockapi.io/vitamins', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(producto)
+      })
+      if (!respuesta.ok) {
+        throw new Error('Error al agregar producto')
+      }
+      const data = await respuesta.json()
+      alert('Producto agregado correctamente')
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className='container_admin'>
+      <nav className='admin_nav'>
+        <Link to='/'><h1><img className='logo' src={logo} alt='logo liv' /></h1></Link>
+        <div className='admin_nav-links'>
+          <button onClick={() => setOpen(true)}><FontAwesomeIcon icon={faPlus} />Agregar nuevo producto</button>
+          {open && (<FormularioProducto onAgregar={agregarProducto} />)}
+          <Link to='/'><FontAwesomeIcon icon={faArrowRightFromBracket} />Salir</Link>
+        </div>
+      </nav>
+
+      <h2>Panel Administrativo</h2>
+
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <div className='admin-products'>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>
+                <div className='admin_products-left'>
+                  <img src={product.imgUrl} alt={product.name} />
+                </div>
+                <div className='admin_products-right'>
+                  <div className='admin-products-data'>
+                    <span><h3>Nombre:</h3><p>{product.name}</p></span>
+                    <span><h3>Product Id:</h3><p>{product.id}</p></span>
+                    <span><h3>Promo:</h3><p>{product.promo}</p></span>
+                    <span><h3>Precio:</h3><p>$ {product.price}</p></span>
+                    <span><h3>Stock:</h3><p>{product.stock} u.</p></span>
+                    <span><h3>Descripción:</h3><p>{product.description}</p></span>
+                    <span><h3>Ingredientes:</h3><p>{product.ingredients}</p></span>
+                    <span><h3>Uso:</h3><p>{product.use}</p></span>
+                    <span><h3>Categoría:</h3><p>{product.category}</p></span>
+                  </div>
+                  <div className='admin-products-btns'>
+                    <button className='btn-negro btn-160'><FontAwesomeIcon icon={faPen} />Editar</button>
+                    <button className='btn-gris btn-160' ><FontAwesomeIcon icon={faTrash} />Eliminar</button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
     </div>
   )
 }
