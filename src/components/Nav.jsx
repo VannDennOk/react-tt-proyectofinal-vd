5f42f9eed3e616b2025ae695ff5f46d33e5f5304
Vanina Denegri <denegri.vanina@gmail.com>
2025-06-15 01:11:36 -0300

    Cambios para NavLink

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './styles/Nav.css';
 
 const Nav = () => {
   return (
     <nav className='nav_container'>
       <ul className='nav_list'>
-        <li><Link className='nav_link' to='/'>Inicio</Link></li>
-        <li><Link className='nav_link' to='/productos'>Productos</Link></li>
-        <li><Link className='nav_link' to='/nosotros'>Nosotros</Link></li>
-        <li><Link className='nav_link' to='/contacto'>Contacto</Link></li>
+        <li><NavLink className='nav_link' to='/'>Inicio</NavLink></li>
+        <li><NavLink className='nav_link' to='/productos'>Productos</NavLink></li>
+        <li><NavLink className='nav_link' to='/nosotros'>Nosotros</NavLink></li>
+        <li><NavLink className='nav_link' to='/contacto'>Contacto</NavLink></li>
       </ul>
     </nav>
   );
