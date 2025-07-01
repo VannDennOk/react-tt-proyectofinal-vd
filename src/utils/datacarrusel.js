4901603d5a59e9d7cf4b05325d699f29aaaa20cf
Vanina Denegri <denegri.vanina@gmail.com>
2025-07-01 01:49:04 -0300

    mueve archivos

--- src/utils/datacarrusel.js
+++ src/utils/datacarrusel.js
@@ -1,24 +1,29 @@
+import galleryDescanso from '../assets/Img/galleryDescanso.png'
+import galleryFuerza from '../assets/Img/galleryFuerza.png'
+import galleryEquilibrio from '../assets/Img/galleryEquilibrio.png'
+import galleryDefensa from '../assets/Img/galleryDefensa.png'
+
 export const datacarrusel = [
     {
         originalx: "https://i.postimg.cc/yN62FgLN/Descanso.png",
-        original: "src/assets/Img/galleryDescanso.png",
+        original: galleryDescanso,
         originalAlt: "imagen de producto descanso"
     
     },
     {
         originalx: "https://i.postimg.cc/P5QBpzzc/Fuerza.png",
-        original: "src/assets/Img/galleryDescanso.png",
+        original: galleryFuerza,
         originalAlt: "imagen de producto fuerza"
     
     },
     {
         originalx: "https://i.postimg.cc/rwkBwYWx/Equilibrio.png",
-        original: "src/assets/Img/galleryDescanso.png",
+        original: galleryEquilibrio,
         originalAlt: "imagen de producto equilibrio"
     },
     {
         originalx: "https://i.postimg.cc/SNC0qpXm/Defensa.png",
-        original: "src/assets/Img/galleryDescanso.png",
+        original: galleryDefensa,
         originalAlt: "imagen de producto defensa"
     }
 ]
